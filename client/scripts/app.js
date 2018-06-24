c49a2c23a03d5b3abac610ffa47e9a708fd10d26
jlukenoff <jlukenoff@gmail.com>
2018-06-23 17:19:53 -0700

    final product

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,55 +6,86 @@ var app = {
   },
   
   /* ------AJAX request methods----- */
+  fetch: function() {
+    $.ajax({
+      url: app.server,
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        // debugger;
+        app.clearMessages();
 
-  //handles sending messages to server
+        let messageData = data.results;
+        
+        var allRooms = messageData.map(obj => obj.roomname);
+
+        // if (app.currentRoom !== undefined) {
+        //   app.currentRooms.push(app.currentRoom);
+        // }
+        
+        var uniqueRooms = _.uniq(allRooms);
+        
+
+        $('#roomSelect').empty();
+        
+        _.each(uniqueRooms, function(room) {
+          room = app.messageFilter(room);                   
+          app.currentRooms.push(room);
+          app.renderRoom(room);
+        });
+        app.renderRoom('New Room...');
+        app.currentRoom = app.currentRoom || app.currentRooms[0];
+        console.log(app.currentRoom);
+        $('#roomSelect').val(app.currentRoom);
+
+        var messages = messageData.filter(function(obj) {
+          return obj.roomname === app.currentRoom;
+        });
+        _.each(messages, function(message) {
+          message.text = app.messageFilter(message.text);
+          message.username = app.messageFilter(message.username); 
+          app.renderMessage(message);
+        });
+        $('span.username').on('click', app.handleUsernameClick);
+        setTimeout(app.fetch, 15000);
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+
   send: function(message) {  
     $.ajax({
-    // This is the url you should use to communicate with the parse API server.
       url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message sent');
-        //app.clearMessages();
-        //app.fetch();
       },
       error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message', data);
       }
     });  
   },
   
-  
-  
-  //gets list of current rooms from server and mount to #roomSelect
-  currentRooms: [], 
-  
-  //handles submission of user input message
   handleSubmit: function() {
-    //retrieves message text from input field
     let $message = $('.submit').val();
     $('.submit').val('');
-    //parses text into userData object
     var messageData = {
       username: window.location.search.slice(10),
       text: $message,
       roomname: app.currentRoom
     };
-    // renders message to DOM
     $('#chats').prepend($(`<div class="chat"><span class="username">${messageData.username}:</span><p>${messageData.text}</p></div>`));
-    //sends userData object to server
     app.send(messageData);
   },
-  
-  //clears chats from DOM
+
   clearMessages: function() {
     $('#chats').empty();
   },
   
-  //mounts individual message to DOM
   renderMessage: function(message) {
     var $node = $(`<div class="chat"><span class="username">${message.username}:</span><p>${message.text}</p></div>`);
     if (app.friends[message.username]) {
@@ -63,18 +94,12 @@ var app = {
     $('#chats').append($node);
   },
   
-  //mounts roomname to DOM
   renderRoom: function(room) {
     $('#roomSelect').append(`<option value="${room}">${room}</option>`);
   },
   
-  //escapes potentially malicious characters
   messageFilter: function(text) {
     return !text ? 'undefined' : text.replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace(/\$/g, '&#36;');
-  },
-  
-  //handles roomname selection
-  selectRoom: function(roomName) {
   },
 
   handleUsernameClick: function() {
@@ -98,6 +123,13 @@ var app = {
       app.currentRoom = newRoom;
       $('#roomSelect').val(newRoom);
       $('#newRoomInputDiv').remove();
+      var messageData = {
+        username: window.location.search.slice(10),
+        text: 'Welcome to ' + app.currentRoom,
+        roomname: app.currentRoom
+      };
+      app.send(messageData);
+      app.fetch();
       
     });
   },
@@ -105,68 +137,9 @@ var app = {
   /* ------Properties----- */
   server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages?order=-createdAt',
   
-  //currentRoom: null,
-
   friends: {},
-  
-  //gets new message data from server
-  fetch: function() {
-    $.ajax({
-      url: app.server,
-      type: 'GET',
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('refreshed');
 
-        app.clearMessages();
-
-        let messageData = data.results;
-
-        // pulls roomNames from list of raw message data
-        
-        var allRooms = messageData.map(obj => obj.roomname);
-
-        if (app.currentRoom !== undefined) {
-          app.currentRooms.push(app.currentRoom);
-        }
-        //filters unique roomnames
-        var uniqueRooms = _.uniq(allRooms);
-        
-        // app.currentRooms = ['New Room...', app.currentRoom];
-
-        $('#roomSelect').empty();
-        // app.renderRoom('New Room...');
-        
-        //mounts each roomName to #roomSelect div
-        _.each(uniqueRooms, function(room) {
-          room = app.messageFilter(room);                   
-          //checks if room is currently selected room
-          app.currentRooms.push(room);
-          app.renderRoom(room);
-        });
-        app.renderRoom('New Room...');
-        app.currentRoom = app.currentRoom || app.currentRooms[0];
-        console.log(app.currentRoom);
-        $('#roomSelect').val(app.currentRoom);
-
-        //filters messages without matching roomName
-        var messages = messageData.filter(function(obj) {
-          return obj.roomname === app.currentRoom;
-        });
-        //mounts each message to DOM
-        _.each(messages, function(message) {
-          message.text = app.messageFilter(message.text);
-          message.username = app.messageFilter(message.username); 
-          app.renderMessage(message);
-        });
-        $('span.username').on('click', app.handleUsernameClick);
-        setTimeout(app.fetch, 15000);
-      },
-      error: function (data) {
-        console.error('chatterbox: Failed to send message', data);
-      }
-    });
-  }
+  currentRooms: [], 
  
 };
 
@@ -188,7 +161,6 @@ $(document).ready(function() {
     app.clearMessages();
     app.fetch();
   });
-
   
 
 });
